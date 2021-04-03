00be6efd64bfbbea1c8291b70595c8bc2ee45cf8
Dylan <dcren2010@gmail.com>
2021-04-02 22:19:31 -0400

    recursion fn finished

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+
+  const flatten = arr => {
+    let flatArr = [];
+    arr.forEach(i => {
+      if (Array.isArray(i)) {
+        flatArr = flatArr.concat(flatten(i));
+      } else {
+        flatArr.push(i);
+      }
+    });
+    return flatArr;
+  };
+
+  let sumArr = flatten(array);
+  if (sumArr.length === 1) {
+    return sumArr[0];
+  }
+  return sumArr[0] + sumItems(sumArr.slice(1));
 }
 
 module.exports = sumItems;
